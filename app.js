ae0a49478f9e50f126a01642142c7d3c62be04fa
Vincent Tsai <vincentvtt@hotmail.com>
2021-08-03 01:00:18 +1000

    feat: refactor encryption to hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +40,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({ email: email }, (err, foundUser) => {
     if (err) {
       console.log(err);
@@ -59,7 +58,7 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   const newUser = new User({
     email: email,
     password: password,
