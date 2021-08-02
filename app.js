fe36a53fdfc5e773afff08d5ca97f2f749a4ea8e
Vincent Tsai <vincentvtt@hotmail.com>
2021-08-03 00:32:55 +1000

    feat: move secret to env variable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,7 +7,6 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
-const secret = "thisisareallylongstringforencrypting";
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
